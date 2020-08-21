eeed5bf427fc756ced8e55728a06e79e4533ea78
Camilo Teixeira de Melo <camilotxm@outlook.com>
2020-08-21 17:21:07 -0300

    Inclusão da logica

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # liquid-template-engine-sample
-PoC para utilização do Liquid template
+PoC para utilização do Liquid template.
+
+O resultado é exibido no console.
---
+++ Sample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using DotLiquid;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Sample
+{
+    class Program
+    {
+
+        public static readonly string pathRoot = Path.GetFullPath(@"../data");
+        public static readonly string templateFile = pathRoot+"/template.html";
+        public static readonly string requestFile = pathRoot+"/request.json";
+
+        static void Main(string[] args) {
+
+            IDictionary<string, object> expandoDict = new Dictionary<string, object>(JsonExpandoObject());
+
+
+            Hash hashes = Hash.FromDictionary(expandoDict);
+            
+            Template template = Template.Parse( TemplateText() );
+            string template_rendered = template.Render(hashes);
+
+            Console.Write( template_rendered );
+        }
+
+        public static ExpandoObject JsonExpandoObject() 
+            => JsonConvert.DeserializeObject<ExpandoObject>(
+                File.ReadAllText(requestFile), new ExpandoObjectConverter()
+            );
+        
+
+        public static string TemplateText() 
+            => File.ReadAllText(templateFile);
+        
+
+
+    }
+}
